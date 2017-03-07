9f05cca5ddd9302a706b3f2d98b800d999428661
--unset <--unset>
2017-03-07 09:29:48 +0800

    number edit

---
+++ src/number/HappyNumber_202.java
@@ -0,0 +1,7 @@
+package number;
+
+/**
+ * Created by ludao on 2017/3/6.
+ */
+public class HappyNumber_202 {
+}
---
+++ src/number/UglyNumber_263.java
@@ -0,0 +1,7 @@
+package number;
+
+/**
+ * Created by ludao on 2017/3/7.
+ */
+public class UglyNumber_263 {
+}
