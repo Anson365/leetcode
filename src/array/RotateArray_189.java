455b512ed8da2505569cfbf2bafaa92e19ac6894
Anson <297856005@qq.com>
2017-06-07 22:34:43 +0800

    189

--- src/array/RotateArray_189.java
+++ src/array/RotateArray_189.java
@@ -8,25 +8,25 @@ import java.util.List;
  */
 public class RotateArray_189 {
     public void rotate(int[] nums, int k) {
-        StringBuilder stringBuilder = new StringBuilder();
-        for(int num:nums){
-            stringBuilder.append(num).append(",");
-        }
-        StringBuilder temp = stringBuilder.reverse();
-        int index = 0;
-        for(int i=0;i<k;i++){
-            index = temp.indexOf(",",index);
-        }
-        String tempStr = temp.substring(index)+temp.substring(0, index);
-        String[] strArray = new StringBuilder(tempStr).reverse().toString().split(",");
-        for(int i=0;i<nums.length;i++){
-            nums[i] = Integer.valueOf(strArray[i]);
+        k = k%nums.length;
+        doReverse(nums, 0, nums.length - 1);
+        doReverse(nums, 0, k - 1);
+        doReverse(nums, k, nums.length - 1);
+    }
+
+    public void doReverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
         }
     }
 
     public static void main(String[] args){
-        int[] nums = {1,2,3,4,5,6,7};
-        int k = 2;
+        int[] nums = {1};
+        int k = 0;
         RotateArray_189 rotateArray_189 = new RotateArray_189();
         rotateArray_189.rotate(nums,k);
     }
