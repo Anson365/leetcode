aa90882e180a387153bf77bdb1503566014c0070
--unset <--unset>
2017-06-11 21:23:27 +0800

    interview edit

---
+++ src/interview/array/PrintArrayInDiagonalDLine.java
@@ -0,0 +1,29 @@
+package interview.array;
+
+/**
+ * Created by ludao on 2017/6/11.
+ */
+public class PrintArrayInDiagonalDLine {
+
+    public void doPrint(int[][] array,int n){
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j+i<n;j++){
+                System.out.print(array[j][j+i]+",");
+            }
+            System.out.println();
+        }
+
+        for(int i=1;i<n;i++){
+            for(int j=0;j+i<n;j++){
+                System.out.print(array[j+i][j]+",");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args){
+        int[][] array = {{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20},{21,22,23,24,25}};
+        PrintArrayInDiagonalDLine printArrayInDiagonalDLine = new PrintArrayInDiagonalDLine();
+        printArrayInDiagonalDLine.doPrint(array,5);
+    }
+}
