778df0e6899ad31972cd2a63460941074cb07dc1
Anson <297856005@qq.com>
2017-08-02 23:41:00 +0800

    647

---
+++ src/dp/PalindromicSubstrings_647.java
@@ -0,0 +1,19 @@
+package dp;
+
+/**
+ * Created by Anson Luo(Anson.Luo@pegasus.ae) on 2017/8/1.
+ */
+public class PalindromicSubstrings_647 {
+    public int countSubstrings(String s) {
+        int n = s.length();
+        int res = 0;
+        boolean[][] dp = new boolean[n][n];
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                dp[i][j] = s.charAt(i) == s.charAt(j) && (j - i < 3 || dp[i + 1][j - 1]);
+                if(dp[i][j]) ++res;
+            }
+        }
+        return res;
+    }
+}
