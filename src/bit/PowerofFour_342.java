91f648b881af74ffd8a3ac68cf786e5c0daa826b
Anson <297856005@qq.com>
2017-09-08 11:00:56 +0800

    342

---
+++ src/bit/PowerofFour_342.java
@@ -0,0 +1,17 @@
+package bit;
+
+/**
+ * Created by Anson Luo(Anson.Luo@pegasus.ae) on 2017/9/8.
+ */
+public class PowerofFour_342 {
+    public boolean isPowerOfFour(int num) {
+        if(num<1){
+            return false;
+        }
+        return Math.log10(num)/Math.log10(4)%1==0;
+    }
+    public static void main(String[] args){
+        PowerofFour_342 powerofFour_342 = new PowerofFour_342();
+        powerofFour_342.isPowerOfFour(8);
+    }
+}
