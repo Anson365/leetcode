7dc0eaaca55cb0511361a740f9000d1d0f2cd632
Anson <297856005@qq.com>
2018-09-03 17:35:20 +0800

    390 & 665

---
+++ src/array/Non_decreasing_Array_665.java
@@ -0,0 +1,39 @@
+package array;
+
+/**
+ * Created by Anson Luo(Anson.Luo@pegasus.ae) on 9/3/18.
+ */
+public class Non_decreasing_Array_665 {
+    public boolean checkPossibility(int[] nums) {
+        Boolean flag = false;
+        for (int i = 0; i + 1< nums.length; i++) {
+            if (nums[i] > nums[i + 1]) {
+                if (flag) {
+                    return false;
+                }
+                boolean current = true, behind = true;
+                if (i - 1 >= 0) {
+                    if (nums[i - 1] > nums[i + 1]) {
+                        current = false;
+                    }
+                }
+                if (i + 1 < nums.length - 1) {
+                    if (nums[i] > nums[i + 2]) {
+                        behind = false;
+                    }
+                }
+                if (!(current || behind)) {
+                    return false;
+                }
+                flag = true;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,3,3,2,4};
+        Non_decreasing_Array_665 non_decreasing_array_665 = new Non_decreasing_Array_665();
+        non_decreasing_array_665.checkPossibility(nums);
+    }
+}
---
+++ src/others/Elimination_Game_390.java
@@ -0,0 +1,31 @@
+package others;
+
+/**
+ * Created by Anson Luo(Anson.Luo@pegasus.ae) on 8/29/18.
+ */
+public class Elimination_Game_390 {
+    public int lastRemaining(int n) {
+        int step = 1;
+        int remaining = n;
+        int head = 1;
+        boolean startFromLeft = true;
+        while (remaining / 2 != 0) {
+            if (startFromLeft) {
+                head += step;
+            } else {
+                if (remaining % 2 == 1) {
+                    head += step;
+                }
+            }
+            step *= 2;
+            remaining /= 2;
+            startFromLeft = !startFromLeft;
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        Elimination_Game_390 elimination_game_390 = new Elimination_Game_390();
+        elimination_game_390.lastRemaining(10);
+    }
+}
