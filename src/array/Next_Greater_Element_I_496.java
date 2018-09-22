999d6512aa8c55ec11df686efc5465d79a9201c6
Anson <297856005@qq.com>
2018-09-22 09:26:26 +0800

    496 temp_792

---
+++ src/array/Next_Greater_Element_I_496.java
@@ -0,0 +1,40 @@
+package array;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Anson Luo(Anson.Luo@pegasus.ae) on 9/22/18.
+ */
+public class Next_Greater_Element_I_496 {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] result = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            int target = nums1[i];
+            boolean flag = false, found = false;
+            for (int j = 0; j < nums2.length; j++) {
+                int temp = nums2[j];
+                if (temp == target) {
+                    flag = true;
+                    continue;
+                }
+                if (temp > target && flag) {
+                    result[i] = temp;
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                result[i] = -1;
+            }
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        Next_Greater_Element_I_496 next_greater_element_i_496 = new Next_Greater_Element_I_496();
+        int[] nums1 = {4,1,2};
+        int[] nums2 = {1,3,4,2};
+        next_greater_element_i_496.nextGreaterElement(nums1, nums2);
+    }
+}
---
+++ src/array/Number_Of_Matching_Subsequences_792.java
@@ -0,0 +1,64 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by Anson Luo(Anson.Luo@pegasus.ae) on 9/21/18.
+ */
+public class Number_Of_Matching_Subsequences_792 {
+    public int numMatchingSubseq(String S, String[] words) {
+        int result = 0;
+        List<MiddleClass> list = new ArrayList<>();
+        for (String word : words) {
+            if (helper(S, word, list)) {
+                result++;
+            }
+        }
+        return result;
+    }
+
+    private boolean helper (String s, String t, List<MiddleClass> list) {
+        int tStart = 0, index = 0;
+        for (int i = 0; i < list.size() && tStart < t.length(); i++) {
+            if (t.charAt(tStart) == list.get(i).value) {
+                tStart++;
+                index = i;
+            }
+        }
+        if (tStart == t.length()) {
+            return true;
+        }
+        for (int i = index + 1; i < s.length() && tStart < t.length(); i++) {
+            if (t.charAt(tStart) == s.charAt(i)) {
+                index = addList(index, s, i, list);
+                tStart++;
+            }
+        }
+        return tStart == t.length();
+
+    }
+
+    private int addList(int index, String s, int charIndex, List<MiddleClass> list) {
+        for (int i = index; i < list.size(); i++) {
+            MiddleClass temp = list.get(i);
+            if (temp.index > index) {
+                list.add(i, new MiddleClass(s.charAt(charIndex), charIndex));
+                return i;
+            }
+        }
+        list.add(new MiddleClass(s.charAt(charIndex), charIndex));
+        return list.size();
+    }
+
+    class MiddleClass {
+        private char value;
+        private int index;
+
+        public MiddleClass(char value, int index) {
+            this.value = value;
+            this.index = index;
+        }
+    }
+}
