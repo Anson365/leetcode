15fd617fbc662a7115cf82054a8fc87251cae34f
Anson <297856005@qq.com>
2018-09-23 11:54:30 +0800

    908

---
+++ src/contest/contest_18_09_23_103/Smallest_Range_I_908.java
@@ -0,0 +1,27 @@
+package contest.contest_18_09_23_103;
+
+import java.util.Arrays;
+
+/**
+ * Created by Anson Luo(Anson.Luo@pegasus.ae) on 9/23/18.
+ */
+public class Smallest_Range_I_908 {
+    public int smallestRangeI(int[] A, int K) {
+        if(A == null || A.length < 2) {
+            return 0;
+        }
+        int max =Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int temp : A) {
+            max = temp > max ? temp : max;
+            min = temp < min ? temp : min;
+        }
+        int start = min + K;
+        int end = max - K;
+        if (start >= end) {
+            return 0;
+        } else {
+            return end - start;
+        }
+    }
+}
