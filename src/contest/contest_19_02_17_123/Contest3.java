03c5bbc44930d879e2f8db3c3eeaa3242ab2e1f1
luodaihua <luodaihua@kuaishou.com>
2019-02-17 13:19:05 +0800

    contest 123

---
+++ src/contest/contest_19_02_17_123/Contest3.java
@@ -0,0 +1,40 @@
+package contest.contest_19_02_17_123;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author luodaihua
+ * Created on 2019-02-17
+ */
+public class Contest3 {
+    public int minKBitFlips(int[] A, int K) {
+        int count = 0;
+        for (int i = 0; i <= A.length - K; i++) {
+            if (A[i] != 1) {
+                flip(A, i, K);
+                count++;
+            }
+        }
+        for (int a : A) {
+            if (a != 1) {
+                return -1;
+            }
+        }
+        return count;
+    }
+
+    private void flip(int[] A, int start, int K) {
+        for (int i = start; i < start + K; i++) {
+            A[i] = A[i] ^ 1;
+        }
+        return;
+    }
+    public static void main(String[] args) {
+        Contest3 contest3 = new Contest3();
+        contest3.minKBitFlips(new int[]{0, 1, 0}, 1);
+    }
+}
---
+++ src/contest/contest_19_02_17_123/Cousins_in_Binary_Tree_993.java
@@ -0,0 +1,41 @@
+package contest.contest_19_02_17_123;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import BaseConstruction.TreeNode;
+
+/**
+ * @author luodaihua
+ * Created on 2019-02-17
+ */
+public class Cousins_in_Binary_Tree_993 {
+    public boolean isCousins(TreeNode root, int x, int y) {
+        Map<Integer, int[]> map = new HashMap<>();
+        helper(map, root, 0);
+        int[] left = map.get(x);
+        int[] right = map.get(y);
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        return left[0] != right[0] && left[1] == right[1];
+    }
+
+    private void helper(Map<Integer, int[]> map, TreeNode root, int depth) {
+        if (root == null) {
+            return;
+        }
+        int parent = root.val;
+        if (root.left != null) {
+            map.put(root.left.val, new int[] {parent, depth + 1});
+            helper(map, root.left, depth + 1);
+        }
+        if (root.right != null) {
+            map.put(root.right.val, new int[] {parent, depth + 1});
+            helper(map, root.right, depth + 1);
+        }
+    }
+}
