71ad4c05bd902154685ddb5b25ebc6832d01a452
luodaihua <luodaihua@kuaishou.com>
2019-02-23 23:31:08 +0800

    473

---
+++ src/dp/Matchsticks_to_Square_473.java
@@ -0,0 +1,38 @@
+package dp;
+
+import java.util.Arrays;
+
+/**
+ * @author luodaihua
+ * Created on 2019-02-23
+ */
+public class Matchsticks_to_Square_473 {
+    public boolean makesquare(int[] nums) {
+        if (nums == null || nums.length < 4) {
+            return false;
+        }
+        int sum = Arrays.stream(nums).sum();
+        if (sum % 4 != 0) {
+            return false;
+        }
+        return dfs(nums, new int[4], 0, sum/4);
+    }
+
+    private boolean dfs(int[] nums, int[] sums, int index, int target) {
+        if (index == nums.length) {
+            return sums[0] == target && sums[1] == target && sums[2] == target;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            if (sums[i] + nums[index] > target) {
+                continue;
+            }
+            sums[i] += nums[index];
+            if (dfs(nums, sums, index + 1, target)) {
+                return true;
+            }
+            sums[i] -= nums[index];
+        }
+        return false;
+    }
+}
